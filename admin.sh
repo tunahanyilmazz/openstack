d0934a3574a6d876fe8539b84525c07f52e966d1
tunahan yilmaz <tunahanyilmazz@gmail.com>
2024-07-03 10:18:25 +0300

    Create admin.sh

---
+++ admin.sh
@@ -0,0 +1,9 @@
+cat <<EOF > admin-openrc.sh
+export OS_USERNAME=alyabby
+export OS_PASSWORD=F@tal123 
+export OS_PROJECT_NAME=admin
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_AUTH_URL=http://192.168.12.114:5000/v3
+export OS_IDENTITY_API_VERSION=3
+EOF
